235bc3260a1a768737675342edf3629139c29a4b
bummings <eabummings@gmail.com>
2024-08-26 09:30:33 -0400

    init

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/demo/demoApp/wtfRestPackage/WTFRestController.java
@@ -0,0 +1,13 @@
+package com.demo.demoApp.wtfRestPackage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WTFRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello bruh???";
+    }
+}
