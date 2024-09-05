12c5b0792d6e59b57c7dade8d2916c6682ccf8dc
bummings <eabummings@gmail.com>
2024-09-05 11:47:33 -0400

    begin thymeleaf review

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/fauxkno/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/fauxkno/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,25 @@ package com.fauxkno.cruddemo.dao;
 
 import com.fauxkno.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    // step one: add method in the DAO interface
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    // int so that it returns the number of rows deleted
+    int deleteAll();
+
+
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/fauxkno/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/fauxkno/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.fauxkno.cruddemo.dao;
 
 import com.fauxkno.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -27,5 +30,51 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        // Student here is not the name of the database table, JPQL syntax is based on entity name and entity fields
+        // lastName also is not the database name, but rather the JPA entity
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student ORDER BY lastName asc", Student.class);
+
+        // return query result
+        return theQuery.getResultList();
+    }
+
+    // step two: implement the method you added in the DAO interface
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        // :theData represents a JPQL named param, prefixed with a colon
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameters- named param is theData, the value provided in is theLastName
+        theQuery.setParameter("theData", theLastName);
+
+        // return query result
+        return theQuery.getResultList();
+    }
+
+    // step two: implement the method you added in the DAO interface
+    @Override
+    @Transactional // since we are modifying
+    public void update(Student theStudent) {
+            entityManager.merge(theStudent);
+        }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student theStudent = entityManager.find(Student.class ,id);
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowsDeleted;
     }
 }
