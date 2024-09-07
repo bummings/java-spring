2c239fe78d0b3f5e9e2162f6c37139cfdd012a0e
bummings <eabummings@gmail.com>
2024-09-07 15:10:36 -0400

    process form v3

--- 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.fauxkno.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -11,13 +15,45 @@ public class HelloWorldController {
         return "helloworld-form";
     }
 
-    // need a controller method to process form input
-    @RequestMapping("/processform")
+    @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
     }
 
+    // need a controller method to process form input
     // need a controller method to read form data
-    // add data to the model
-    
+
+    @RequestMapping("/processForm2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // read the request param from the html form
+        String theName = request.getParameter("studentName");
+
+        // convert data to all caps
+        theName = theName.toUpperCase();
+
+        // create message
+        String result = "Yo, " + theName;
+
+        // add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processForm3")
+    public String processFormV3(@RequestParam("studentName") String theName,
+                                Model model) {
+
+        // convert data to all caps
+        theName = theName.toUpperCase();
+
+        // create message
+        String result = "Say, " + theName;
+
+        // add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
 }
