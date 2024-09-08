02d5dea4c90258e9269e8cf43326839f04bdb798
bummings <eabummings@gmail.com>
2024-09-08 14:06:49 -0400

    back to normal here in 188

--- 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/model/Student.java
@@ -1,11 +1,15 @@
 package com.fauxkno.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
+
+    private List<String> favoriteSystems;
 
     public Student() {
 
@@ -42,6 +46,15 @@ public class Student {
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
     }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+
 
 }
 
