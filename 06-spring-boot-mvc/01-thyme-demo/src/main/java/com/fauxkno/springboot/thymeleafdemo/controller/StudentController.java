33e1dc39006ce2f4d8589ba999de9b1fe5690fba
bummings <eabummings@gmail.com>
2024-09-08 00:24:31 -0400

    radio buttons

--- 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.fauxkno.springboot.thymeleafdemo.controller;
 
 import com.fauxkno.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -18,6 +24,9 @@ public class StudentController {
 
         // add created student as attribute into the model
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
