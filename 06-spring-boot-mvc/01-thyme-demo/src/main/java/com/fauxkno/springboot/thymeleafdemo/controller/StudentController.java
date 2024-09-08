5f803c95759d32a88804808a01fdae13d7a8ac00
bummings <eabummings@gmail.com>
2024-09-08 15:30:11 -0400

    ready to start validation

--- 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-mvc/01-thyme-demo/src/main/java/com/fauxkno/springboot/thymeleafdemo/controller/StudentController.java
@@ -20,6 +20,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // create a student object
@@ -33,6 +36,9 @@ public class StudentController {
 
         // add the list of languages to the model (from application.properties)
         theModel.addAttribute("languages", languages);
+
+        // add the list of systems to the model (from application.properties)
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }
