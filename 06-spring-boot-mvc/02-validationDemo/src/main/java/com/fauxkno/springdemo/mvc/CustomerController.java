25e179e13b6ff8e86c2486c46c35587e9e7a7e0b
bummings <eabummings@gmail.com>
2024-09-09 15:17:14 -0400

    ok thru 197

--- 06-spring-boot-mvc/02-validationDemo/src/main/java/com/fauxkno/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-mvc/02-validationDemo/src/main/java/com/fauxkno/springdemo/mvc/CustomerController.java
@@ -1,8 +1,12 @@
 package com.fauxkno.springdemo.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -12,4 +16,14 @@ public class CustomerController {
         return "customer-form";
     }
 
+    @PostMapping("/processForm")
+    // these validation rules have already been setup in the Customer class
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
+    }
 }
