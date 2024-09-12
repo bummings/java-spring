ed2ddd3b3f9fbe685af81e33ea2bb34d128746ec
bummings <eabummings@gmail.com>
2024-09-12 11:54:30 -0400

    clickin

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafDemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafDemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafDemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -41,5 +43,13 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+
+        // use a redirect to prevent duplicate submissions
     }
 }
